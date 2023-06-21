9364c2203896e4294f85a5e0c630523867dfc298
geoffwithers <geoffwithers91@gmail.com>
2023-06-21 18:34:43 -0400

    final commit, added README

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
-# Weather-Dashboard
-5 day weather forecasts for cities using a server-side API
+# Weather Forecast Dashboard 
+![Github license](https://img.shields.io/badge/license--blue.svg)
+## Description
+This application allows the user to search any city and find current weather information, as well as a five day weather forecast.
+## Deployed Application URL
+https://geoffwithers.github.io/Weather-Dashboard/
+## Screenshot
+![alt-text](./img/screenshot.png)
+## Table of Contents
+* [Features](#features)
+* [Languages & Dependencies](#languagesanddependencies)
+* [How to Use This Application](#HowtoUseThisApplication)
+* [Contributors](#contributors)
+* [Testing](#testing)
+* [Questions](#questions)
+## Features
+This project links to openweather.org's API, and saves a history of your past searches.
+## Languages & Dependencies
+N/A
+## How to Use This Application:
+HTML, CSS, JavaScript, APIs, jquery
+## Contributors
+N/A
+## Testing
+N/A
+## Questions
+Please send your questions [here](mailto:geoffwithers91@gmail.com?subject=[GitHub]%20Dev%20Connect) or visit [github/geoffwithers](https://github.com/geoffwithers).
--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 // Global variables
 var searchHistory = [];
+    if (localStorage.getItem('city')) {
+        searchHistory = JSON.parse(localStorage.getItem('city'))
+    }
 var weatherApiRoomUrl = 'https://api.openweathermap.org'
 var weatherApiKey = '8b51c69ac57300c69947dc2afe72a71e';
 
@@ -9,12 +12,40 @@ var searchBtn = document.getElementById('search-btn');
 var searchText = document.getElementById('input');
 var currentWeather = document.getElementById('current-container');
 var forecastWeather = document.getElementById('forecast-container');
+var searchHistoryContainer = document.getElementById('history');
+
 
 searchBtn.addEventListener('click', searchCity)
 function searchCity() {
     getWeatherData(searchText.value)
     getWeatherForecast(searchText.value)
 }
+
+function renderSearchHistory() {
+    searchHistoryContainer.innerHTML = '';
+    for (let i = 0; i < searchHistory.length; i++) {
+        searchHistoryContainer.innerHTML += `<button class="btn">${searchHistory[i]}</button>`
+    }
+    var btn = document.querySelectorAll('.btn')
+    for (let i = 0; i < btn.length; i++) {
+        btn[i].addEventListener('click', btnSearch)
+        
+    }
+}
+
+renderSearchHistory()
+
+function btnSearch() {
+    getWeatherData(this.textContent)
+    getWeatherForecast(this.textContent)
+}
+
+getWeatherData(searchHistory[searchHistory.length - 1])
+getWeatherForecast(searchHistory[searchHistory.length - 1])
+
+
+
+
 
 function getWeatherData(cityName) {
     var url = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${weatherApiKey}&units=imperial`
@@ -24,6 +55,12 @@ function getWeatherData(cityName) {
     })
     .then(function(data) {
         console.log(data)
+        if (searchHistory.includes(data.name) === false && data.name) {
+            searchHistory.push(data.name)
+            localStorage.setItem('city', JSON.stringify(searchHistory))
+            renderSearchHistory() 
+        }
+        
     currentWeather.innerHTML = 
         `<h2>${data.name} ${dayjs.unix(data.dt).format('(MM/DD/YYYY)')}<img src='https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png'></h2>
         <p>Temp: ${data.main.temp} F</p>
@@ -41,7 +78,7 @@ function getWeatherForecast(cityName) {
     .then(function(data) {
         console.log(data)
         forecastWeather.innerHTML = ""
-        for (let i = 0; i < data.list.length; i=i+8) {
+        for (let i = 3; i < data.list.length; i=i+8) {
             forecastWeather.innerHTML = forecastWeather.innerHTML + `
             <div>
             <h3>${dayjs.unix(data.list[i].dt).format('MM/DD/YYYY')}</h3>
@@ -54,3 +91,4 @@ function getWeatherForecast(cityName) {
         }
     })
 };
+
